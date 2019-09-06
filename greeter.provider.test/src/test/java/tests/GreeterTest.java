1134a68c2a54056b61d73b060b8425b3d3aaff0c
plugins <plugins@lingocoder.com>
2019-09-06 05:03:19 +0200

    Modular Multi-Release Jar File Gradle Plugin Demo

---
+++ greeter.provider.test/src/test/java/tests/GreeterTest.java
@@ -0,0 +1,32 @@
+/**
+ * A Gradle plugin that supports compiling, testing, assembling and maintaining Modular Multi-Release JAR Files.
+ *
+ * Copyright (C) 2019 lingocoder <plugins@lingocoder.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package tests;
+
+import examples.greeter.api.Greeter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class GreeterTest {
+    @Test
+    public void testLocate() {
+        Greeter greeter = new GreeterLocator().findGreeter();
+        assertFalse(greeter.hello().isBlank());
+    }
+}
